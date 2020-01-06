8716952e4eec7af634021d6dca1ac0ab665c97f9
godmonth <link2people@gmail.com>
2020-01-06 13:07:20 +0400

    update i18nerrorcode

---
+++ commons-rpc/src/main/java/io/bitexpress/topia/commons/rpc/error/i18n/I18nErrorCode.java
@@ -0,0 +1,12 @@
+package io.bitexpress.topia.commons.rpc.error.i18n;
+
+import io.bitexpress.topia.commons.rpc.error.ErrorCode;
+
+/**
+ * <p></p >
+ *
+ * @author shenyue
+ */
+public interface I18nErrorCode extends ErrorCode {
+    String getMessageKey();
+}
