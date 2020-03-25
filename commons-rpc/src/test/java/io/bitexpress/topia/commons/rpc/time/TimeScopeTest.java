e8b64e3ca4986deacfd8a47de67d83f7e76d2916
godmonth <link2people@gmail.com>
2020-03-25 12:23:55 +0400

    增加timescope

--- commons-rpc/src/test/java/io/bitexpress/topia/commons/rpc/time/TimeScopeTest.java
+++ commons-rpc/src/test/java/io/bitexpress/topia/commons/rpc/time/TimeScopeTest.java
@@ -14,7 +14,7 @@ class TimeScopeTest {
     @Test
     void name() {
         TimePoint startTime = TimePoint.builder().inclusive(true).time(DateUtils.addDays(new Date(), -1)).build();
-        TimePoint endTime = TimePoint.builder().inclusive(false).time(DateUtils.addDays(new Date(), -1)).build();
+        TimePoint endTime = TimePoint.builder().inclusive(false).time(DateUtils.addDays(new Date(), 0)).build();
 
         TimeScope timeScope = TimeScope.builder().startTime(startTime).endTime(endTime).build();
         System.out.println(timeScope);
