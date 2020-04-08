88bc142da4e6686af1b0713e8ad889a0ccfc23e0
godmonth <link2people@gmail.com>
2020-04-08 12:39:26 +0400

    update version add i18nMessageresponse

--- commons-basic/src/main/java/io/bitexpress/topia/commons/basic/rpc/utils2/BaseResponseUtils.java
+++ commons-basic/src/main/java/io/bitexpress/topia/commons/basic/rpc/utils2/BaseResponseUtils.java
@@ -1,13 +1,13 @@
 package io.bitexpress.topia.commons.basic.rpc.utils2;
 
 import io.bitexpress.topia.commons.basic.exception.ErrorCodeException;
+import io.bitexpress.topia.commons.basic.exception.i18n.I18nErrorCodeException;
 import io.bitexpress.topia.commons.rpc.BusinessCode;
 import io.bitexpress.topia.commons.rpc.SystemCode;
 import io.bitexpress.topia.commons.rpc.response.BaseResponse;
 import io.bitexpress.topia.commons.rpc.response.ResponseHeader;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.Validate;
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -38,8 +38,17 @@ public class BaseResponseUtils {
     }
 
     public static BaseResponse errorCodeExceptionResponse(ErrorCodeException e) {
+        if (e instanceof I18nErrorCodeException) {
+            return i18nErrorCodeExceptionResponse((I18nErrorCodeException) e);
+        }
         logger.info("code:{},message:{}", e.getErrorCode(), e.getMessage());
         ResponseHeader responseHeader = ResponseHeader.builder().systemCode(SystemCode.SUCCESS).businessCode(e.getErrorCode()).message(e.getMessage()).build();
+        return BaseResponse.builder().header(responseHeader).build();
+    }
+
+    public static BaseResponse i18nErrorCodeExceptionResponse(I18nErrorCodeException e) {
+        logger.info("code:{},message:{},i18nMessage:{}", e.getErrorCode(), e.getMessage(), e.getI18nMessage());
+        ResponseHeader responseHeader = ResponseHeader.builder().systemCode(SystemCode.SUCCESS).businessCode(e.getErrorCode()).message(e.getMessage()).i18nMessage(e.getI18nMessage()).build();
         return BaseResponse.builder().header(responseHeader).build();
     }
 
@@ -55,11 +64,16 @@ public class BaseResponseUtils {
     }
 
     public static void parse(BaseResponse baseResponse, String... silentBusinessCodes) {
-		ResponseHeader header = baseResponse.getHeader();
-		Validate.validState(header.getSystemCode() == SystemCode.SUCCESS, header.getMessage());
+        ResponseHeader header = baseResponse.getHeader();
+        Validate.validState(header.getSystemCode() == SystemCode.SUCCESS, header.getMessage());
         String[] successCodeArray = ArrayUtils.add(silentBusinessCodes, BusinessCode.SUCCESS.name());
         if (!ArrayUtils.contains(successCodeArray, header.getBusinessCode())) {
-            throw new ErrorCodeException(header.getBusinessCode(), header.getMessage());
+            if (header.getI18nMessage() != null) {
+                throw new I18nErrorCodeException(header.getBusinessCode(), header.getMessage(), header.getI18nMessage());
+            } else {
+                throw new ErrorCodeException(header.getBusinessCode(), header.getMessage());
+            }
+
         }
     }
 
