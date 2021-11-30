b72dee7bd129ee40c9ea8614536e707c0eba626e
godmonth <link2people@gmail.com>
2021-11-30 11:15:03 +0800

    update deprecated note

--- commons-rpc/src/main/java/io/bitexpress/topia/commons/rpc/ResultResponse.java
+++ commons-rpc/src/main/java/io/bitexpress/topia/commons/rpc/ResultResponse.java
@@ -5,33 +5,34 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 
 /**
  * 包含处理数据的响应组件
- * 
- * @author shenyue
  *
  * @param <T>
+ * @author shenyue
+ * @see io.bitexpress.topia.commons.rpc.response.BodyResponse
+ * @deprecated
  */
 @Deprecated
 public class ResultResponse<T> extends BaseResponse {
-	/**
-	 * 返回结果
-	 */
-	private T result;
+    /**
+     * 返回结果
+     */
+    private T result;
 
-	public T getResult() {
-		return result;
-	}
+    public T getResult() {
+        return result;
+    }
 
-	public void setResult(T result) {
-		this.result = result;
-	}
+    public void setResult(T result) {
+        this.result = result;
+    }
 
-	/**
-	 * @see java.lang.Object#toString()
-	 */
-	public String toString() {
-		return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
-				.appendSuper(super.toString()).append("result", this.result)
-				.toString();
-	}
+    /**
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .appendSuper(super.toString()).append("result", this.result)
+                .toString();
+    }
 
 }
