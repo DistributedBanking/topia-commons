13354cac2b2b445aeea5708c145625fda2fb6354
shenyue <yue.shen@126.com>
2024-06-29 16:55:42 +0800

    update

--- commons-basic/src/main/java/io/bitexpress/topia/commons/basic/rpc/utils2/MapBodyResponseUtils.java
+++ commons-basic/src/main/java/io/bitexpress/topia/commons/basic/rpc/utils2/MapBodyResponseUtils.java
@@ -1,8 +1,9 @@
 package io.bitexpress.topia.commons.basic.rpc.utils2;
 
 import io.bitexpress.topia.commons.rpc.BusinessCode;
-import io.bitexpress.topia.commons.rpc.MapResultResponse;
 import io.bitexpress.topia.commons.rpc.SystemCode;
+import io.bitexpress.topia.commons.rpc.response.MapBodyResponse;
+import io.bitexpress.topia.commons.rpc.response.ResponseHeader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,22 +17,17 @@ import java.util.Map;
 public class MapBodyResponseUtils {
     private static final Logger logger = LoggerFactory.getLogger(MapBodyResponseUtils.class);
 
-    public static <K, V> MapResultResponse<K, V> codeMapResultResponse(Map<K, V> result, SystemCode systemCode,
-                                                                       String businessCode, String message) {
-        MapResultResponse<K, V> rr = new MapResultResponse<K, V>();
-        rr.setSystemCode(systemCode);
-        rr.setBusinessCode(businessCode);
-        rr.setMessage(message);
-        rr.setResult(result);
-        return rr;
+    public static <K, V> MapBodyResponse<K, V> codeMapResponse(Map<K, V> body, SystemCode systemCode, String businessCode, String message) {
+        ResponseHeader responseHeader = ResponseHeader.builder().systemCode(systemCode).businessCode(businessCode).message(message).build();
+        return MapBodyResponse.<K, V>mapBodyBuilder().header(responseHeader).body(body).build();
     }
 
-    public static <K, V> MapResultResponse<K, V> successMapResultResponse(Map<K, V> result) {
-        return codeMapResultResponse(result, SystemCode.SUCCESS, BusinessCode.SUCCESS.name(), null);
+    public static <K, V> MapBodyResponse<K, V> successMapResponse(Map<K, V> result) {
+        return codeMapResponse(result, SystemCode.SUCCESS, BusinessCode.SUCCESS.name(), null);
     }
 
-    public static <K, V> MapResultResponse<K, V> failureMapResultResponse(String message) {
-        return codeMapResultResponse(null, SystemCode.FAILURE, null, message);
+    public static <K, V> MapBodyResponse<K, V> failureMapResponse(String message) {
+        return codeMapResponse(null, SystemCode.FAILURE, null, message);
     }
 
 
